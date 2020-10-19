0f5a28326cb98c6d76b75d228c1896a9b1d4476a
Erich Ess <erich.g.ess@gmail.com>
2020-10-19 10:10:30 -0400

    implement parsing for basic arithmetic expressions

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,20 @@ fn main() {
     println!("{:?}", ast);
     let program = assembly::Program::compile(&ast);
     program.print();
+
+    let text = "2 * 3 + 4";
+    let tokens = Token::tokenize(&text);
+    println!("{:?}", tokens);
+    let tokens = tokens
+        .into_iter()
+        .filter(|t| t.is_ok())
+        .map(|t| t.unwrap())
+        .collect();
+    let ast = Node::parse(tokens);
+    println!("{:?}", ast);
+
+    let program = assembly::Program::compile(&ast.unwrap());
+    program.print();
 }
 
 fn new_ast<'a>() -> Node {
@@ -33,26 +47,30 @@ fn new_ast<'a>() -> Node {
     ast
 }
 
-/*
-    Grammar
-    NUMBER := 0-9*
-    FACTOR := NUMBER | (EXPRESSION)
-    TERM := FACTOR [* FACTOR]
-    EXPRESSION :=  EXPRESSION [+ TERM]
-    BIND := let IDENTIFIER = EXPRESSION;
-
-    tokenize - takes a string of text and converts it to a string of tokens
-    parse - takes a string of tokens and converts it into an AST
-    compile - takes an AST and converts it to assembly
-*/
-
 // Token - a type which captures the different types of tokens and which is output
 // by tokenize
 #[derive(Debug)]
-enum Token {
+pub enum Token {
     Integer(i32),
     Mul,
     Add,
+}
+
+impl Token {
+    /// Split a string into tokens, first by splitting by whitespace and then
+    /// for each substring determining if it is an operator or an integer.
+    pub fn tokenize(text: &str) -> Vec<Result<Token, &str>> {
+        let ss = text.split_ascii_whitespace();
+        ss.map(|s| match s.parse::<i32>() {
+            Ok(i) => Ok(Token::Integer(i)),
+            Err(_) => match s {
+                "*" => Ok(Token::Mul),
+                "+" => Ok(Token::Add),
+                _ => Err("Invalid token"),
+            },
+        })
+        .collect()
+    }
 }
 
 // AST - a type(s) which is used to construct an AST representing the logic of the
@@ -64,6 +82,85 @@ pub struct Node {
     value: Token,
     left: NodeOption,
     right: NodeOption,
+}
+
+impl Node {
+    /*
+        Grammar
+        NUMBER := 0-9*
+        FACTOR := NUMBER | (EXPRESSION)
+        TERM := FACTOR [* TERM]
+        EXPRESSION :=  TERM [+ EXPRESSION]
+        BIND := let IDENTIFIER = EXPRESSION;
+
+        tokenize - takes a string of text and converts it to a string of tokens
+        parse - takes a string of tokens and converts it into an AST
+        compile - takes an AST and converts it to assembly
+    */
+    pub fn parse(tokens: Vec<Token>) -> Option<Node> {
+        let mut iter = tokens.iter().peekable();
+        Node::expression(&mut iter)
+    }
+
+    fn expression(iter: &mut std::iter::Peekable<core::slice::Iter<Token>>) -> Option<Node> {
+        match Node::term(iter) {
+            Some(n) => match iter.peek() {
+                Some(Token::Add) => {
+                    iter.next();
+                    let n2 = Node::expression(iter).expect("An expression after +");
+                    Some(Node {
+                        value: Token::Add,
+                        left: Some(Box::new(n)),
+                        right: Some(Box::new(n2)),
+                    })
+                }
+                _ => Some(n),
+            },
+            None => None,
+        }
+    }
+
+    fn term(iter: &mut std::iter::Peekable<core::slice::Iter<Token>>) -> Option<Node> {
+        match Node::factor(iter) {
+            Some(n) => match iter.peek() {
+                Some(Token::Mul) => {
+                    iter.next();
+                    let n2 = Node::term(iter).expect("a valid term after *");
+                    Some(Node {
+                        value: Token::Mul,
+                        left: Some(Box::new(n)),
+                        right: Some(Box::new(n2)),
+                    })
+                }
+                _ => Some(n),
+            },
+            None => None,
+        }
+    }
+
+    fn factor(iter: &mut std::iter::Peekable<core::slice::Iter<Token>>) -> Option<Node> {
+        match Node::number(iter) {
+            Some(n) => {
+                iter.next();
+                Some(n)
+            }
+            None => None,
+        }
+    }
+
+    fn number(iter: &mut std::iter::Peekable<core::slice::Iter<Token>>) -> Option<Node> {
+        match iter.peek() {
+            Some(token) => match token {
+                Token::Integer(i) => Some(Node {
+                    value: Token::Integer(*i),
+                    left: None,
+                    right: None,
+                }),
+                _ => None,
+            },
+            None => None,
+        }
+    }
 }
 
 // ASM - types capturing the different assembly instructions along with functions to
