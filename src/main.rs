7a0a7c7c8ac20f91828b51b8cb8b62ddb7f65916
Erich Ess <erich.g.ess@gmail.com>
2020-11-02 10:27:27 -0500

    Added CL args to specify source file and output file

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,37 @@ mod lexer;
 mod parser;
 mod type_checker;
 
+use clap::{App, Arg};
 use compiler::*;
 use lexer::Token;
 use parser::*;
 use type_checker::*;
 
 fn main() {
-    let text = "
-        fn my_main( p ) { 
-            c := init my_co(5, 13);
-            w := yield c;
-            x := 1; 
-            println x; 
-            println w; 
-            z := yield c;
-            println z;
-            return x + p; 
-        }
+    let matches = App::new("Braid Compiler")
+        .version("0.1.0")
+        .author("Erich Ess")
+        .about("Compiles Braid language files into x86 assembly for use by the NASM assembler")
+        .arg(
+            Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .takes_value(true)
+                .help("Source code file to compile"),
+        )
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .takes_value(true)
+                .help("Name the output file that the assembly will be written to"),
+        )
+        .get_matches();
 
-        co my_co (x, y) {
-            yret x;
-            return y;
-        }
-        ";
+    let input = matches
+        .value_of("input")
+        .expect("Expected an input source file to compile");
+    let text = std::fs::read_to_string(input).expect("Failed to read input file");
 
     println!("Code: {}", text);
     let tokens = lexer::tokenize(&text);
@@ -45,8 +53,8 @@ fn main() {
     println!("FuncTable: {:?}", func_table);
 
     let program = Compiler::compile(&ast, &mut func_table);
-    let mut output =
-        std::fs::File::create("./target/output.asm").expect("Failed to create output file");
+    let output_target = matches.value_of("output").unwrap_or("./target/output.asm");
+    let mut output = std::fs::File::create(output_target).expect("Failed to create output file");
     //let mut output = std::io::stdout();
     program
         .print(&mut output)
