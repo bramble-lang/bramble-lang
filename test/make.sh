f13dbb8d5f334c97b1b90f778bb27e38779eeabf
Erich Ess <erichgess@localhost.localdomain>
2020-11-03 13:16:38 -0500

    Add some tools to automate end to end testing of the compiler

---
+++ test/make.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+function run_test() {
+    test=$1
+    rm ./target/*
+    cargo run -- -i ./src/${test}.br -o ./target/output.asm 2>&1 > compiler.log
+    nasm -g -f elf32 ./target/output.asm -l ./target/output.lst -o ./target/output.obj > assembler.log
+    gcc -w ./target/output.obj macro.c -g -o ./target/output -m32 2>&1 > gcc.log
+
+    ./target/output > ./target/stdout
+
+    result=$(diff ./target/stdout ./src/${test}.out)
+    if [ $? -eq 0 ]
+    then
+        echo "${test}: Pass"
+    else
+        echo "${test}: Fail"
+        echo ${result}
+    fi
+}
+
+run_test "simple"
+run_test "simple_coroutine"
