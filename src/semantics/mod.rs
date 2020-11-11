8128a8f29c6aece0ad7c06ca0b42344d714166e8
Erich Ess <erichgess@localhost.localdomain>
2020-11-11 07:49:06 -0500

    Add module for semantic analysis

---
+++ src/semantics/mod.rs
@@ -0,0 +1,10 @@
+/*
+ * Handles semantic analysis of a syntax tree.  This includes:
+ * 1. Type checking: determing the type of every expression and making sure that the types match
+ *    any type restrictions.
+ * 2. Checking functions, variables, coroutines, etc. to make sure that they exist
+ * 3. Constructing the symbol table for the code.
+ *
+ * These functions will take an AST that has ParserInfo metadata and will output the AST updated
+ * with SemanticMetadata (including the type of each node and the symbol tables).
+ */
