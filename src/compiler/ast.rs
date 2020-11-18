398cfa36d42c6d4d883408773069025a044b80a4
Erich Ess <erichgess@localhost.localdomain>
2020-11-18 14:18:52 -0500

    Added unit tests.

--- src/compiler/ast.rs
+++ src/compiler/ast.rs
@@ -11,16 +11,32 @@ impl<'a> ScopeStack<'a> {
         }
     }
 
+    /// Push a new scope onto the stack.
     pub fn push(&mut self, scope: &'a Scope) {
         self.stack.push(scope);
     }
 
+    /// Pop the current scope off of the stack
     pub fn pop(&mut self) -> Option<&'a Scope> {
         self.stack.pop()
     }
 
+    /// Searches through the stack, starting at the top and going to the bottom, for a
+    /// variable with the given name.  This will not search past the root scope of the
+    /// current function.
     pub fn find(&self, name: &str) -> Option<&'a Symbol> {
-        self.stack.iter().rev().find_map(|scope| scope.get(name))
+        for scope in self.stack.iter().rev() {
+            let t = scope.get(name);
+            if t.is_some() {
+                return t;
+            }
+            match scope.ty {
+                Type::Block => (),
+                Type::Function{..} => return None,
+            }
+        }
+
+        None
     }
 }
 
@@ -148,5 +164,27 @@ mod tests {
         stack.push(&inner_scope);
 
         assert_eq!(stack.find("y").is_none(), true);
+    }
+
+    #[test]
+    fn test_find_symbol_does_not_pass_function() {
+        let mut stack = ScopeStack::new();
+
+        let mut outer_scope = Scope::new(Type::Block);
+        outer_scope.insert("nope", 4, 4);
+        stack.push(&outer_scope);
+        
+        let mut fun_scope = Scope::new(Type::Function{next_label: 0, allocation: 8});
+        fun_scope.insert("y", 4, 4);
+        fun_scope.insert("z", 4, 8);
+        stack.push(&fun_scope);
+
+        let mut inner_scope = Scope::new(Type::Block);
+        inner_scope.insert("x", 4, 4);
+        stack.push(&inner_scope);
+
+        assert_eq!(stack.find("x").is_some(), true);
+        assert_eq!(stack.find("y").is_some(), true);
+        assert_eq!(stack.find("nope").is_some(), false);
     }
 }
