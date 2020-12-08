b5a98d5c3075c96968c26b294dd73137c46b9607
Erich Ess <erichgess@localhost.localdomain>
2020-12-08 13:19:16 -0500

    added braid test for expression blocks which have local variables of custom type

--- test/test.sh
+++ test/test.sh
@@ -49,6 +49,7 @@ then
     run_test "parser/missing_type"
 
     run_test "structs/basic"
+    run_test "structs/expr_block"
     run_test "structs/expr_block_result"
     run_test "structs/copying"
     run_test "structs/copying_simple"
