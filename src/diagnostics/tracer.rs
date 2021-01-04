e313bd5df60b759191ba0d2544ef2a6a5e02257a
Erich Ess <erichgess@localhost.localdomain>
2021-01-04 08:59:36 -0500

    code clean up

--- src/diagnostics/tracer.rs
+++ src/diagnostics/tracer.rs
@@ -1,42 +1,25 @@
 #[macro_export]
 macro_rules! trace {
     ($ts:expr) => {
-        match &$ts.tracing {
-            &TracingConfig::Only(ln) if ($ts.line() as usize) == ln => println!(
-                "{} <- L{}:{:?}",
-                function_name!(),
-                $ts.line(),
-                $ts.current_token()
-            ),
-            &TracingConfig::Before(ln) if ($ts.line() as usize) <= ln => println!(
-                "{} <- L{}:{:?}",
-                function_name!(),
-                $ts.line(),
-                $ts.current_token()
-            ),
-            &TracingConfig::After(ln) if ($ts.line() as usize) >= ln => println!(
-                "{} <- L{}:{:?}",
-                function_name!(),
-                $ts.line(),
-                $ts.current_token()
-            ),
+        let print_trace = match &$ts.tracing {
+            &TracingConfig::Only(ln) if ($ts.line() as usize) == ln => true,
+            &TracingConfig::Before(ln) if ($ts.line() as usize) <= ln => true,
+            &TracingConfig::After(ln) if ($ts.line() as usize) >= ln => true,
             &TracingConfig::Between(start, end)
                 if ($ts.line() as usize) >= start && ($ts.line() as usize) <= end =>
             {
-                println!(
-                    "{} <- L{}:{:?}",
-                    function_name!(),
-                    $ts.line(),
-                    $ts.current_token()
-                )
+                true
             }
-            &TracingConfig::All => println!(
+            &TracingConfig::All => true,
+            _ => false,
+        };
+        if print_trace {
+            println!(
                 "{} <- L{}:{:?}",
                 function_name!(),
                 $ts.line(),
                 $ts.current_token()
-            ),
-            _ => {}
+            )
         }
     };
 }
