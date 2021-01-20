97edc72dcfad5525e821f26d473ccb61bc1a7805
Erich Gerhard Ess <erich.g.ess@gmail.com>
2021-01-20 17:12:08 -0500

    adding comments

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,3 +1,29 @@
+/**
+ * The Compiler takes an AST that has been semantically analyzed and converts
+ * it into x64 assembly language.
+ * 
+ * Internally, the Compiler runs through a few precompiler steps which calculate
+ * hardware architecture specific data for the program (e.g. assigning byte size
+ * to primitive and custom types, computing the stack frame size, etc.). These
+ * precompilation steps are handled in the `ast` submodule. The final output
+ * of the precompilation stage is an `AST<Scope>`, which is an abstract syntax
+ * tree that has all the architecture specific data for each node.
+ * 
+ * The Compiler type, which actually generates the x64 assembly, will only accept
+ * an `AST<Scope>` as input; because, a tree of this type has been guaranteed to 
+ * be syntactically, semantically, and architecturally validated.  Once it reaches
+ * this point, all information necessary to generate assembly code is already computed
+ * and the input source code has been fully validated, so the compiler can translate
+ * exactly what it is given.
+ * 
+ * Because of this, if any error happens in the Compiler, we assume that the error
+ * cannot be caused by User input; therefore, it must be a bug in the compiler
+ * itself: whether the x64 translation, or the semantic layer allowed invalid code,
+ * or something else. An error in this stage, then, is always considered critical
+ * and unrecoverable, so the policy is to immeidately panic and return to the console
+ * from exactly the point the error was first discovered. So that we are as close 
+ * to the origin as possible.
+ */
 mod ast;
 pub mod compiler;
 mod x86;
