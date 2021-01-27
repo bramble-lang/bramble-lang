cf196a6afbf2f23a31e4579f6711e36877fc1101
Erich Ess <erich.g.ess@gmail.com>
2021-01-27 09:59:36 -0500

    Moved the std/io files for Linux to ./braid/linux/std/io to match the pattern used for Mac

--- test/make.sh
+++ test/make.sh
@@ -11,7 +11,7 @@ run() {
         cargo run -- -p linux "$@" -o ./target/output.asm
         echo ""
         echo "Assembling"
-        nasm -g -f elf64 ../braid/std/io.asm -l ./target/std_io.lst -o ./target/std_io.obj > assembler.log
+        nasm -g -f elf64 ../braid/linux/std/io.asm -l ./target/std_io.lst -o ./target/std_io.obj > assembler.log
         nasm -g -f elf64 ./target/output.asm -l ./target/output.lst -o ./target/output.obj >> assembler.log
         gcc -no-pie -fno-pie -w ./target/std_io.obj ./target/output.obj -g -o ./target/output -m64 2>&1 > gcc.log
         built=1
