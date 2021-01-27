91631ca3fb08e55d99431c591ab4a6ee6f382318
Erich Gerhard Ess <erich.g.ess@gmail.com>
2021-01-27 08:48:34 -0500

    test script checks which OS to target when compiling a Braid program

--- test/make.sh
+++ test/make.sh
@@ -3,19 +3,23 @@
 run() {
     # test=$1
     rm -f ./target/*
-    echo "Compiling"
-    cargo run -- -p machos "$@" -o ./target/output.asm
-    echo ""
-    echo "Assembling"
 
     built=0
 
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        echo "Compiling"
+        cargo run -- -p linux "$@" -o ./target/output.asm
+        echo ""
+        echo "Assembling"
         nasm -g -f elf64 ../braid/std/io.asm -l ./target/std_io.lst -o ./target/std_io.obj > assembler.log
         nasm -g -f elf64 ./target/output.asm -l ./target/output.lst -o ./target/output.obj >> assembler.log
         gcc -no-pie -fno-pie -w ./target/std_io.obj ./target/output.obj -g -o ./target/output -m64 2>&1 > gcc.log
         built=1
     elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "Compiling"
+        cargo run -- -p machos "$@" -o ./target/output.asm
+        echo ""
+        echo "Assembling"
         nasm -g -f macho64 ../braid/macho64/std/io.asm -l ./target/std_io.lst -o ./target/std_io.obj > assembler.log
         nasm -g -f macho64 ./target/output.asm -l ./target/output.lst -o ./target/output.obj >> assembler.log
         gcc -w ./target/std_io.obj ./target/output.obj -g -o ./target/output -m64 2>&1 > gcc.log
--- test/test.sh
+++ test/test.sh
@@ -6,7 +6,11 @@ run_test() {
     rm -rf ./target/*
 
     # cargo run -- -i ./src/${test}.br -o ./target/output.asm > ./target/stdout
-    ../target/debug/braid-lang -p machos -i ./src/${test}.br -o ./target/output.asm > ./target/stdout
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        ../target/debug/braid-lang -p linux -i ./src/${test}.br -o ./target/output.asm > ./target/stdout
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        ../target/debug/braid-lang -p machos -i ./src/${test}.br -o ./target/output.asm > ./target/stdout
+    fi
 
     # If there were no compilation errors then run the assembler and linker
     if [ -f "./target/output.asm" ]
