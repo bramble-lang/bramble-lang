59f0a0faccbebb36c15b041df29d1a354fe2cbd3
Erich Gerhard Ess <erich.g.ess@gmail.com>
2021-01-28 08:39:11 -0500

    Braid test script now automatically finds tests and also prints out the success rate

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+#   Runs all the Braid integration tests.
+#
+#   Each test consists of a Braid program, an expected output, and, sometimes, an input file.
+#   This script will compile the Braid program, run it, and compare the output with the expected
+#   output, if the two match then the test passes.
+#
+#   To add an integration test, simply write a Braid program and save it to `./test/src/{path}/{test}.br`
+#   and put the expected output in `./test/src/{path}/{test}.out`.  This script will automatically
+#   find and execute the test (based on the existance of the .out file).
+#
+#   If a test requires input, then put each input, in order, in a `.in` file with each input on
+#   a separate line.
+
+num_tests=0
+num_pass=0
+
 run_test() {
     test=$1
     input="./src/${test}.in"
@@ -46,6 +62,7 @@ run_test() {
     result=$(diff ./target/stdout ./src/${test}.out)
     if [ $? -eq 0 ]
     then
+        ((num_pass=num_pass+1))
         echo "${test}: Pass"
     else
         echo "${test}: Fail"
@@ -56,68 +73,11 @@ run_test() {
 cargo build
 if [ $? -eq 0 ]
 then
-    run_test "operations/arithmetic"
-    run_test "operations/boolean"
-    run_test "simple"
-    run_test "simple_coroutine"
-    run_test "coroutine_too_many_calls"
-    run_test "coroutine_infinite"
-    run_test "fn/recurse"
-    run_test "scope/out_of_scope"
-    run_test "parser/missing_type"
-    run_test "boolean"
-    run_test "comparison"
-    run_test "boolean_complex"
-    run_test "boolean_coroutine"
-    run_test "coroutine_two"
+    tests=`find ./src | grep "\.out" | sed 's/\.\/src\/\(.*\)\.out/\1/'`
+    for test in ${tests[@]}; do
+        ((num_tests=num_tests+1))
+        run_test $test
+    done
 
-    run_test "exp_block/simple"
-    run_test "exp_block/multi_line"
-    run_test "exp_block/within_exp"
-
-    run_test "if_exp/if"
-    run_test "if_exp/if_block"
-    run_test "if_exp/if_as_conditional"
-    run_test "if_exp/return_in_if"
-
-    run_test "binding/recursive"
-    run_test "binding/undeclared"
-    run_test "binding/before_declaration"
-    run_test "binding/redeclare"
-    run_test "binding/mutation"
-
-    run_test "string_literals/basic"
-    run_test "string_literals/with_functions"
-    run_test "string_literals/with_coroutines"
-
-    run_test "structs/basic"
-    run_test "structs/expr_block"
-    run_test "structs/expr_block_result"
-    run_test "structs/copying"
-    run_test "structs/copying_simple"
-    run_test "structs/big"
-    run_test "structs/nested"
-    run_test "structs/nested_simple"
-    run_test "structs/nested_deep"
-    run_test "structs/passing"
-    run_test "structs/pass_struct_expr"
-    run_test "structs/return_nested"
-    run_test "structs/return_struct_expr"
-    run_test "structs/returning"
-    run_test "structs/coroutine_1"
-    run_test "structs/coroutine_2"
-    run_test "structs/coroutine_3"
-    run_test "structs/coroutine_4"
-    run_test "structs/errors/field"
-    run_test "structs/errors/missing_member"
-    run_test "structs/errors/type_mismatch"
-
-    run_test "modules/simple"
-    run_test "modules/coroutine"
-    run_test "modules/nested"
-    run_test "modules/struct"
-    run_test "modules/return"
-
-    run_test "std/io/readi64"
-    run_test "std/io/writei64"
+    echo "\n${num_pass}/${num_tests} Tests Passed"
 fi
