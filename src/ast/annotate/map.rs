b9bc88d9a116aabbfdb55647551c2c6361f5eabe
Erich Ess <erich.g.ess@gmail.com>
2021-02-11 15:48:12 -0500

    only need the constraint on F in one place

--- src/ast/annotate/map.rs
+++ src/ast/annotate/map.rs
@@ -15,7 +15,7 @@ use super::{super::node::Node, super::parameter::Parameter, Annotation};
  * it to a register.
  */
 
-pub struct Map<A, B, T>
+pub struct Map<A, B, F, T>
 where
     A: Debug + Annotation,
     T: Fn(&A) -> String,
@@ -25,27 +25,27 @@ where
     pub format: T,
     ph: PhantomData<A>,
     ph2: PhantomData<B>,
+    ph3: PhantomData<F>,
 }
 
-impl<A, B, T> Map<A, B, T>
+impl<A, B, F, T> Map<A, B, F, T>
 where
     A: Debug + Annotation,
+    F: FnMut(&A) -> B + Copy,
     T: Fn(&A) -> String,
 {
-    pub fn new(name: &str, tracing: TracingConfig, format: T) -> Map<A, B, T> {
+    pub fn new(name: &str, tracing: TracingConfig, format: T) -> Map<A, B, F, T> {
         Map {
             name: name.into(),
             tracing,
             format,
             ph: PhantomData,
             ph2: PhantomData,
+            ph3: PhantomData,
         }
     }
 
-    pub fn for_module<F>(&self, m: &Module<A>, mut f: F) -> Module<B>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    pub fn for_module(&self, m: &Module<A>, mut f: F) -> Module<B> {
         if self.tracing != TracingConfig::Off {
             println!("{}", self.name);
         }
@@ -57,46 +57,36 @@ where
             m2.add_module(self.for_module(child_module, f));
         }
 
-        m2.get_functions_mut().append(&mut self.for_items(m.get_functions(), f));
-        m2.get_coroutines_mut().append(&mut self.for_items(m.get_coroutines(), f));
-        m2.get_structs_mut().append(&mut self.for_items(m.get_structs(), f));
+        m2.get_functions_mut()
+            .append(&mut self.for_items(m.get_functions(), f));
+        m2.get_coroutines_mut()
+            .append(&mut self.for_items(m.get_coroutines(), f));
+        m2.get_structs_mut()
+            .append(&mut self.for_items(m.get_structs(), f));
 
         m2
     }
 
-    fn for_items<F>(&self, items: &Vec<Item<A>>, f: F) -> Vec<Item<B>>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    fn for_items(&self, items: &Vec<Item<A>>, f: F) -> Vec<Item<B>> {
         let mut v = vec![];
         for i in items.iter() {
             v.push(match i {
-                Item::Struct(sd) => {
-                    Item::Struct(self.for_structdef(sd, f))
-                }
-                Item::Routine(rd) => {
-                    Item::Routine(self.for_routinedef(rd, f))
-                }
+                Item::Struct(sd) => Item::Struct(self.for_structdef(sd, f)),
+                Item::Routine(rd) => Item::Routine(self.for_routinedef(rd, f)),
             });
             self.tracer(i);
         }
         v
     }
 
-    fn for_structdef<F>(&self, sd: &StructDef<A>, mut f: F) -> StructDef<B>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    fn for_structdef(&self, sd: &StructDef<A>, mut f: F) -> StructDef<B> {
         let b = f(sd.annotation());
         self.tracer(sd);
         let fields = self.for_parameters(&sd.fields, f);
         StructDef::new(sd.get_name(), b, fields)
     }
 
-    fn for_routinedef<F>(&self, rd: &RoutineDef<A>, mut f: F) -> RoutineDef<B>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    fn for_routinedef(&self, rd: &RoutineDef<A>, mut f: F) -> RoutineDef<B> {
         let b = f(rd.annotation());
         // loop through all the params
         let params = self.for_parameters(&rd.params, f);
@@ -108,7 +98,7 @@ where
         }
 
         self.tracer(rd);
-        RoutineDef{
+        RoutineDef {
             name: rd.name.clone(),
             def: rd.def,
             annotations: b,
@@ -118,10 +108,7 @@ where
         }
     }
 
-    fn for_parameters<F>(&self, params: &Vec<Parameter<A>>, mut f: F) -> Vec<Parameter<B>>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    fn for_parameters(&self, params: &Vec<Parameter<A>>, mut f: F) -> Vec<Parameter<B>> {
         let mut nparams = vec![];
         for p in params {
             let b = f(p.annotation());
@@ -131,75 +118,49 @@ where
         nparams
     }
 
-    fn for_statement<F>(&self, statement: &Statement<A>, f: F) -> Statement<B>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    fn for_statement(&self, statement: &Statement<A>, f: F) -> Statement<B> {
         let s = match statement {
-            Statement::Bind(b) => {
-                Statement::Bind(box self.for_bind(b, f))
-            }
-            Statement::Mutate(m) => {
-                Statement::Mutate(box self.for_mutate(m, f))
-            }
-            Statement::Return(r) => {
-                Statement::Return(box self.for_return(r, f))
-            }
-            Statement::YieldReturn(yr) => {
-                Statement::YieldReturn(box self.for_yieldreturn(yr, f))
-            }
-            Statement::Expression(e) => {
-                Statement::Expression(box self.for_expression(e, f))
-            }
+            Statement::Bind(b) => Statement::Bind(box self.for_bind(b, f)),
+            Statement::Mutate(m) => Statement::Mutate(box self.for_mutate(m, f)),
+            Statement::Return(r) => Statement::Return(box self.for_return(r, f)),
+            Statement::YieldReturn(yr) => Statement::YieldReturn(box self.for_yieldreturn(yr, f)),
+            Statement::Expression(e) => Statement::Expression(box self.for_expression(e, f)),
         };
         self.tracer(statement);
         s
     }
 
-    fn for_bind<F>(&self, bind: &Bind<A>, mut f: F) -> Bind<B>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    fn for_bind(&self, bind: &Bind<A>, mut f: F) -> Bind<B> {
         let b = f(bind.annotation());
         let rhs = self.for_expression(bind.get_rhs(), f);
-        Bind::new(b, bind.get_id(), bind.get_type().clone(), bind.is_mutable(), rhs)
+        Bind::new(
+            b,
+            bind.get_id(),
+            bind.get_type().clone(),
+            bind.is_mutable(),
+            rhs,
+        )
     }
 
-    fn for_mutate<F>(&self, mutate: &Mutate<A>, mut f: F) -> Mutate<B>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    fn for_mutate(&self, mutate: &Mutate<A>, mut f: F) -> Mutate<B> {
         let b = f(mutate.annotation());
         let rhs = self.for_expression(mutate.get_rhs(), f);
         Mutate::new(b, mutate.get_id(), rhs)
     }
 
-    fn for_yieldreturn<F>(&self, yr: &YieldReturn<A>, mut f: F) -> YieldReturn<B>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    fn for_yieldreturn(&self, yr: &YieldReturn<A>, mut f: F) -> YieldReturn<B> {
         let b = f(yr.annotation());
-        let value = yr.get_value()
-            .as_ref()
-            .map(|rv| self.for_expression(rv, f));
+        let value = yr.get_value().as_ref().map(|rv| self.for_expression(rv, f));
         YieldReturn::new(b, value)
     }
 
-    fn for_return<F>(&self, r: &Return<A>, mut f: F) -> Return<B>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    fn for_return(&self, r: &Return<A>, mut f: F) -> Return<B> {
         let b = f(r.annotation());
-        let value = r.get_value()
-            .as_ref()
-            .map(|rv| self.for_expression(rv, f));
+        let value = r.get_value().as_ref().map(|rv| self.for_expression(rv, f));
         Return::new(b, value)
     }
 
-    fn for_expression<F>(&self, exp: &Expression<A>, mut f: F) -> Expression<B>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    fn for_expression(&self, exp: &Expression<A>, mut f: F) -> Expression<B> {
         use Expression::*;
 
         match exp {
@@ -210,7 +171,9 @@ where
             Expression::CustomType(annotation, name) => CustomType(f(annotation), name.clone()),
             Expression::Identifier(annotation, id) => Identifier(f(annotation), id.clone()),
             Path(annotation, path) => Path(f(annotation), path.clone()),
-            Expression::IdentifierDeclare(annotation, id, p) => IdentifierDeclare(f(annotation), id.clone(), p.clone()),
+            Expression::IdentifierDeclare(annotation, id, p) => {
+                IdentifierDeclare(f(annotation), id.clone(), p.clone())
+            }
             MemberAccess(..) => self.for_member_access(exp, f),
             UnaryOp(..) => self.for_unary_op(exp, f),
             BinaryOp(..) => self.for_binary_op(exp, f),
@@ -222,13 +185,8 @@ where
         }
     }
 
-    fn for_expression_block<F>(&self, block: &Expression<A>, mut f: F) -> Expression<B>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
-        if let Expression::ExpressionBlock(annotation, body, final_exp) =
-            block
-        {
+    fn for_expression_block(&self, block: &Expression<A>, mut f: F) -> Expression<B> {
+        if let Expression::ExpressionBlock(annotation, body, final_exp) = block {
             let b = f(annotation);
 
             let mut nbody = vec![];
@@ -243,10 +201,7 @@ where
         }
     }
 
-    fn for_member_access<F>(&self, access: &Expression<A>, mut f: F) -> Expression<B>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    fn for_member_access(&self, access: &Expression<A>, mut f: F) -> Expression<B> {
         if let Expression::MemberAccess(annotation, src, member) = access {
             let b = f(annotation);
             let src = self.for_expression(src, f);
@@ -256,10 +211,7 @@ where
         }
     }
 
-    fn for_unary_op<F>(&self, un_op: &Expression<A>, mut f: F) -> Expression<B>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    fn for_unary_op(&self, un_op: &Expression<A>, mut f: F) -> Expression<B> {
         if let Expression::UnaryOp(annotation, op, operand) = un_op {
             let b = f(annotation);
             let operand = self.for_expression(operand, f);
@@ -269,10 +221,7 @@ where
         }
     }
 
-    fn for_binary_op<F>(&self, bin_op: &Expression<A>, mut f: F) -> Expression<B>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    fn for_binary_op(&self, bin_op: &Expression<A>, mut f: F) -> Expression<B> {
         if let Expression::BinaryOp(annotation, op, l, r) = bin_op {
             let b = f(annotation);
             let l = self.for_expression(l, f);
@@ -283,10 +232,7 @@ where
         }
     }
 
-    fn for_if<F>(&self, if_exp: &Expression<A>, mut f: F) -> Expression<B>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    fn for_if(&self, if_exp: &Expression<A>, mut f: F) -> Expression<B> {
         if let Expression::If {
             annotation,
             cond,
@@ -298,16 +244,18 @@ where
             let cond = self.for_expression(cond, f);
             let if_arm = self.for_expression(if_arm, f);
             let else_arm = else_arm.as_ref().map(|ea| box self.for_expression(ea, f));
-            Expression::If{annotation: b, cond: box cond,if_arm:  box if_arm, else_arm}
+            Expression::If {
+                annotation: b,
+                cond: box cond,
+                if_arm: box if_arm,
+                else_arm,
+            }
         } else {
             panic!("Expected IfExpression, but got {:?}", if_exp)
         }
     }
 
-    fn for_routine_call<F>(&self, rc: &Expression<A>, mut f: F) -> Expression<B>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    fn for_routine_call(&self, rc: &Expression<A>, mut f: F) -> Expression<B> {
         if let Expression::RoutineCall(annotation, call, name, params) = rc {
             let b = f(annotation);
             let mut nparams = vec![];
@@ -320,10 +268,7 @@ where
         }
     }
 
-    fn for_yield<F>(&self, yield_exp: &Expression<A>, mut f: F) -> Expression<B>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    fn for_yield(&self, yield_exp: &Expression<A>, mut f: F) -> Expression<B> {
         if let Expression::Yield(annotation, e) = yield_exp {
             let b = f(annotation);
             let e = self.for_expression(e, f);
@@ -333,10 +278,7 @@ where
         }
     }
 
-    fn for_struct_expression<F>(&self, se: &Expression<A>, mut f: F) -> Expression<B>
-    where
-        F: FnMut(&A) -> B + Copy,
-    {
+    fn for_struct_expression(&self, se: &Expression<A>, mut f: F) -> Expression<B> {
         if let Expression::StructExpression(annotation, struct_name, fields) = se {
             let b = f(annotation);
             let mut nfields = vec![];
