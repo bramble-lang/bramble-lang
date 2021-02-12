cde3f42d512fc9c15cdd801a122ca091afa64976
Erich Ess <erich.g.ess@gmail.com>
2021-02-12 12:36:38 -0500

    Implemented a post order iterator

--- src/ast/annotate/iter.rs
+++ src/ast/annotate/iter.rs
@@ -6,24 +6,39 @@ pub struct PostOrderIter<'a, A>
 where
     A: std::fmt::Debug + Annotation,
 {
-    stack: Vec<&'a dyn Node<A>>,
     out: Vec<&'a dyn Node<A>>,
 }
 
+/**
+Performs a Post Order DFS traversal over all the nodes in an AST.
+
+TODO: This will do the full traversal when `new` is called, this can be
+improved to amortize time while reducing storage costs by lazily populating
+`out`
+*/
 impl<'a, A> PostOrderIter<'a, A> where A: std::fmt::Debug + Annotation {
     pub fn new(node: &'a dyn Node<A>) -> PostOrderIter<'a, A> {
+        let mut stack = vec![node];
+        let mut out = vec![];
+
+        // Build a Post Ordered vector of references to Nodes
+        while let Some(n) = stack.pop() {
+            out.push(n);
+            let children = n.children();
+            for c in children {
+                stack.push(c);
+            }
+        }
+
         PostOrderIter {
-            stack: vec![node],
-            out: vec![],
+            out,
         }
     }
+}
 
-    pub fn next(&mut self) -> Option<&'a dyn Node<A>> {
-        match self.stack.pop() {
-            Some(n) => (),
-            None => ()
-        }
-
-        None
+impl<'a, A> Iterator for PostOrderIter<'a, A> where A: std::fmt::Debug + Annotation {
+    type Item = &'a dyn Node<A>;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.out.pop()
     }
 }
