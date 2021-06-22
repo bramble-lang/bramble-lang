47671e377ef42fec42781b1ba0743cdbc4adbd86
Erich Gerhard Ess <erich.g.ess@gmail.com>
2021-06-22 10:19:43 -0400

    Moved functions related to working with a project into their own src file

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,17 @@ mod io;
 mod lexer;
 mod llvm;
 mod parser;
+mod project;
+mod result;
 mod semantics;
 
-use std::{
-    path::{Path, PathBuf},
-    process::exit,
-};
+use std::{path::Path, process::exit};
 
 use cli::*;
 use diagnostics::config::TracingConfig;
 use inkwell::context::Context;
-use io::get_files;
-use lexer::tokens::Token;
+use project::*;
 use semantics::type_resolver::*;
-
-use crate::ast::Module;
 
 const BRAID_FILE_EXT: &str = "br";
 const ROOT_MODULE_NAME: &str = "root";
@@ -35,13 +31,19 @@ fn main() {
         .value_of("input")
         .expect("Expected an input source file to compile");
     let src_path = Path::new(input);
-    let src_input = read_src_files(&src_path);
+    let src_input = read_src_files(&src_path, BRAID_FILE_EXT);
 
     let trace_lexer = TracingConfig::parse(config.value_of("trace-lexer"));
     let token_sets = tokenize_project(src_input, trace_lexer);
 
     let trace_parser = TracingConfig::parse(config.value_of("trace-parser"));
-    let root = parse_project(ROOT_MODULE_NAME, token_sets, trace_parser);
+    let root = match parse_project(ROOT_MODULE_NAME, token_sets, trace_parser) {
+        Ok(root) => root,
+        Err(msg) => {
+            println!("Compilation failed: {}", msg);
+            exit(ERR_PARSER_ERROR)
+        }
+    };
 
     // Type Check
     let trace_semantic_node = TracingConfig::parse(config.value_of("trace-semantic-node"));
@@ -83,183 +85,5 @@ fn main() {
 
         llvm.emit_object_code(Path::new(output_target)).unwrap();
     } else {
-    }
-}
-
-fn create_module_path<'a>(
-    module: &'a mut Module<u32>,
-    path: &[String],
-) -> Option<&'a mut Module<u32>> {
-    match path.split_first() {
-        Some((head, rest)) => {
-            if module.get_module(head).is_none() {
-                let sub = Module::new(head, 0);
-                module.add_module(sub);
-            }
-
-            let sub = module
-                .get_module_mut(head)
-                .expect("A module with this name was just created and ought to be found");
-
-            if rest.len() == 0 {
-                Some(sub)
-            } else {
-                create_module_path(sub, rest)
-            }
-        }
-        None => None,
-    }
-}
-
-type Project<T> = Vec<CompilationUnit<T>>;
-
-struct CompilationUnit<T> {
-    path: Vec<String>,
-    data: T,
-}
-
-/// Given the location of source file(s) this function will read the file
-/// or files and return a list of the text of all source files. For each
-/// file there will also be the associated path to the file (relative to
-/// `src_path`).
-///
-/// If `src_path` is a directory, this will recursively read every file in
-/// that directory and its subdirectories.  If it is a file, it will read
-/// only that file.
-fn read_src_files(src_path: &Path) -> Vec<CompilationUnit<String>> {
-    let files =
-        get_files(&src_path, BRAID_FILE_EXT).expect(&format!("Could not open: {:?}", src_path));
-    let mut texts: Vec<CompilationUnit<String>> = vec![];
-    for file in files {
-        let p = file_path_to_module_path(&file, &src_path);
-
-        let text = std::fs::read_to_string(&file)
-            .expect(&format!("Failed to read input file: {:?}", file));
-        texts.push(CompilationUnit {
-            path: p,
-            data: text,
-        });
-    }
-
-    texts
-}
-
-/// Parses every tokenized compilation unit in the given vector.
-/// Each compilation unit is parsed into a module named after the
-/// path given in the CompilationUnit and all are added as child
-/// modules of a single "root" module.
-fn parse_project(
-    root_module: &str,
-    token_sets: Project<Vec<Token>>,
-    trace_parser: TracingConfig,
-) -> Module<u32> {
-    parser::parser::set_tracing(trace_parser);
-    let mut root = Module::new(root_module, 0);
-    for src_tokens in token_sets {
-        match parse_src_tokens(src_tokens) {
-            Ok(ast) => append_module(&mut root, ast),
-            Err(msg) => {
-                println!("{}", msg);
-                exit(1)
-            }
-        }
-    }
-    root
-}
-
-fn tokenize_project(
-    src_input: Project<String>,
-    trace_lexer: TracingConfig,
-) -> Vec<CompilationUnit<Vec<Token>>> {
-    let mut token_sets = vec![];
-    for src in src_input {
-        let src_tokens = tokenize_src_file(src, trace_lexer);
-        token_sets.push(src_tokens);
-    }
-    token_sets
-}
-
-fn tokenize_src_file(
-    src: CompilationUnit<String>,
-    trace_lexer: TracingConfig,
-) -> CompilationUnit<Vec<Token>> {
-    let mut lexer = crate::lexer::lexer::Lexer::new(&src.data);
-    lexer.set_tracing(trace_lexer);
-    let tokens = lexer.tokenize();
-    let tokens: Vec<Token> = tokens
-        .into_iter()
-        .filter(|t| match t {
-            Ok(_) => true,
-            Err(msg) => {
-                println!("{}", msg);
-                false
-            }
-        })
-        .map(|t| t.unwrap())
-        .collect();
-    CompilationUnit {
-        path: src.path,
-        data: tokens,
-    }
-}
-
-/// Takes CompilationUnit which has been tokenized and parses the tokens into
-/// an AST.
-///
-/// The last element of the compilation unit's path (the name of the module derived
-/// from the source file name).  Will be removed from the path vector, because it becomes
-/// part of the data field (when a module is created with the same name that becomes the
-/// parent of all items within the source file).
-fn parse_src_tokens(
-    src_tokens: CompilationUnit<Vec<Token>>,
-) -> Result<CompilationUnit<Module<u32>>, String> {
-    if let Some((name, parent_path)) = src_tokens.path.split_last() {
-        match parser::parser::parse(name, &src_tokens.data) {
-            Ok(Some(ast)) => Ok(CompilationUnit {
-                path: parent_path.to_owned(),
-                data: ast,
-            }),
-            Ok(None) => Err("Critical: no AST was generated by the parser".into()),
-            Err(msg) => Err(format!("Error: {}", msg)),
-        }
-    } else {
-        Err("Invalid compilation unit path: was empty".into())
-    }
-}
-
-fn append_module(root: &mut Module<u32>, src_ast: CompilationUnit<Module<u32>>) {
-    let parent = if src_ast.path.len() == 0 {
-        root
-    } else {
-        create_module_path(root, &src_ast.path).unwrap()
-    };
-    parent.add_module(src_ast.data);
-}
-
-fn file_path_to_module_path(file: &PathBuf, src_path: &Path) -> Vec<String> {
-    let fpath = file.as_path();
-    let base = if src_path.is_dir() {
-        src_path
-    } else {
-        src_path
-            .parent()
-            .expect("Given a file which is also the root of the directory structure.")
-    };
-
-    let rel_path = fpath.strip_prefix(&base).unwrap();
-
-    let mut p: Vec<String> = rel_path
-        .iter()
-        .map(|e| e.to_str().expect("File name was not valid unicode").into())
-        .collect();
-
-    truncate_extension(&mut p, ".br");
-    p
-}
-
-fn truncate_extension(path: &mut Vec<String>, ext: &str) {
-    match path.last_mut() {
-        Some(l) if l.ends_with(ext) => l.truncate(l.len() - ext.len()),
-        _ => (),
     }
 }
