880e445ac5dd4bc0839ccd8f9749ef6f29f48bf3
Erich Gerhard Ess <erich.g.ess@gmail.com>
2021-06-26 10:39:43 -0400

    Update the make and test script so that Linux will build and link the new Braid std::io library

--- test/make-llvm.sh
+++ test/make-llvm.sh
@@ -9,11 +9,12 @@ run() {
 
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         echo "Compiling"
+        cargo run -- --llvm -p linux --input "../braid/std" -o ./target/std.obj
         cargo run -- --llvm -p linux "$@" -o ./target/output.obj
         echo ""
         echo "Assembling"
         nasm -g -f elf64 ../braid/linux/llvm/std/io.asm -l ./target/std_io_llvm.lst -o ./target/std_io_llvm.obj > assembler.log
-        gcc -no-pie -fno-pie -w ./target/std_io_llvm.obj ./target/output.obj -g -o ./target/output -m64 2>&1 > gcc.log
+        gcc -no-pie -fno-pie -w ./target/std.obj ./target/std_io_llvm.obj ./target/output.obj -g -o ./target/output -m64 2>&1 > gcc.log
         built=1
     elif [[ "$OSTYPE" == "darwin"* ]]; then
         echo "Compiling"
