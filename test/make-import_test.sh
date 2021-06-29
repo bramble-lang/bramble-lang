bb81d88a37b2036d73be4a2aa8faedbefa87e947
Erich Gerhard Ess <erich.g.ess@gmail.com>
2021-06-29 14:18:14 -0400

    Added a script to run integration tests for importing functions and structures.  This is separate from the other tests because there is additional compilation and linking steps that are needed.

--- test/make-import_test.sh
+++ test/make-import_test.sh
@@ -9,34 +9,28 @@ run() {
 
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         echo "Compiling"
-        cargo run -- --llvm -p linux --input "../braid/std" -o ./target/std.obj --manifest
-        cargo run -- --llvm -p linux --import ./target/std.manifest "$@" -o ./target/output.obj
+        cargo run -- -p linux --input "./import/shared" -o ./target/shared.obj --manifest
+        cargo run -- -p linux --import ./target/shared.manifest --input "./import/indirect" -o ./target/indirect.obj --manifest
+        cargo run -- -p linux --input "../braid/std" -o ./target/std.obj --manifest
+        cargo run -- -p linux --import ./target/std.manifest,./target/shared.manifest,./target/indirect.manifest "$@" -o ./target/output.obj
         echo ""
         echo "Assembling"
         nasm -g -f elf64 ../braid/linux/llvm/std/input.asm -l ./target/std_input.lst -o ./target/std_input.obj > assembler.log
-        gcc -no-pie -fno-pie -w ./target/std.obj ./target/std_input.obj ./target/output.obj -g -o ./target/output -m64 2>&1 > gcc.log
+        gcc -no-pie -fno-pie -w ./target/indirect.obj ./target/shared.obj ./target/std.obj ./target/std_input.obj ./target/output.obj -g -o ./target/output -m64 2>&1 > gcc.log
         built=1
     elif [[ "$OSTYPE" == "darwin"* ]]; then
         echo "Compiling"
-        cargo run -- -p machos --input "./src/import/shared" -o ./target/shared.obj --manifest
+        cargo run -- -p machos --input "./import/shared" -o ./target/shared.obj --manifest
+        cargo run -- -p machos --import ./target/shared.manifest --input "./import/indirect" -o ./target/indirect.obj --manifest
         cargo run -- -p machos --input "../braid/std" -o ./target/std.obj --manifest
-        cargo run -- -p machos --import ./target/std.manifest,./target/shared.manifest "$@" -o ./target/output.obj
+        cargo run -- -p machos --import ./target/std.manifest,./target/shared.manifest,./target/indirect.manifest "$@" -o ./target/output.obj
         echo ""
         echo "Assembling"
         nasm -g -f macho64 ../braid/macho64/llvm/std/input.asm -l ./target/std_input.lst -o ./target/std_input.obj > assembler.log
-        gcc -w ./target/shared.obj ./target/std.obj ./target/std_input.obj ./target/output.obj -g -o ./target/output -m64 2>&1 > gcc.log
+        gcc -w ./target/indirect.obj ./target/shared.obj ./target/std.obj ./target/std_input.obj ./target/output.obj -g -o ./target/output -m64 2>&1 > gcc.log
         built=1
     else
         echo "Unknown OS: ${OSTYPE}"
-    fi
-
-    echo ""
-
-    if [[ $built -eq 1 ]]; then
-        echo "Running"
-        ./target/output
-    else
-        echo "Build Failed"
     fi
 }
 
