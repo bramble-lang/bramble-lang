16aee642e77ab65a022f8fc8d946d7e519c49236
Erich Ess <erich.g.ess@gmail.com>
2021-08-31 15:27:25 -0400

    Added a random source code generator and a test script that will have braidc parse the randomly generated source code. The purpose is to cover all the crazy combinations of syntactically correct code that can be written.
    This does not generate _semantically_ correct code. So braidc will only run to the parsing stage

---
+++ test/syntax-test.sh
@@ -0,0 +1,20 @@
+# Randomly generate syntactically correct code and test that braidc validates it.
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+
+cargo build --release
+COUNTER=0
+until [ $COUNTER -ge $1 ]; do
+    ../target/release/braid-gen 10 10 > /tmp/braid_test.br
+    ../target/release/braidc --stage=parser --platform=linux -i /tmp/braid_test.br --output=./
+    result=$?
+
+    if [[ result -ne 0 ]]; then
+        cat /tmp/braid_test.br
+        exit 1
+    fi
+    let COUNTER+=1
+done
