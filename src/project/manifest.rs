18b9f3cf026544296e6c4d5b7b8339ee9c0b059c
Erich Gerhard Ess <erich.g.ess@gmail.com>
2021-09-10 14:13:55 -0400

    Adding in logic to convert Compiler data to Manifest data

--- src/project/manifest.rs
+++ src/project/manifest.rs
@@ -1,9 +1,12 @@
 use serde::{Deserialize, Serialize};
 
-use crate::compiler::{
-    ast::{Item, Module, Node, Path, RoutineDef, StructDef, Type},
-    import::Import,
-    semantics::semanticnode::SemanticContext,
+use crate::{
+    compiler::{
+        ast::{Item, Module, Node, Path, RoutineDef, StructDef, Type},
+        import::Import,
+        semantics::semanticnode::SemanticContext,
+    },
+    StringTable,
 };
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -80,4 +83,125 @@ impl Manifest {
         // Write manifest to file
         serde_yaml::to_writer(file, self)
     }
+}
+
+struct ManifestRoutine {
+    name: String,
+    canon_path: String,
+    params: Vec<ManifestType>,
+    ret_ty: ManifestType,
+}
+
+impl ManifestRoutine {
+    fn from_rd(st: &StringTable, rd: RoutineDef<SemanticContext>) -> Result<Self, String> {
+        let name = st
+            .get(rd.name)
+            .ok_or(format!("Could not find routine name"))?
+            .into();
+        let params = rd
+            .params
+            .iter()
+            .map(|p| ManifestType::from_ty(st, &p.ty))
+            .collect::<Result<_, String>>()?;
+        let ret_ty = ManifestType::from_ty(st, &rd.ret_ty)?;
+        let canon_path = path_to_string(st, rd.get_context().get_canonical_path())?;
+
+        Ok(ManifestRoutine {
+            name,
+            params,
+            ret_ty,
+            canon_path,
+        })
+    }
+}
+
+struct ManifestStructDef {
+    name: String,
+    canon_path: String,
+    fields: Vec<(String, ManifestType)>,
+}
+
+impl ManifestStructDef {
+    fn from_sd(st: &StringTable, sd: &StructDef<SemanticContext>) -> Result<Self, String> {
+        let name = st
+            .get(sd.get_name())
+            .ok_or(format!("Could not find the name of the struct"))?
+            .into();
+        let canon_path = path_to_string(st, sd.get_context().get_canonical_path())?;
+        let fields = sd
+            .get_fields()
+            .iter()
+            .map(|f| {
+                let name = st.get(f.name).ok_or(format!("Could not find field name"));
+                let fty = ManifestType::from_ty(st, &f.ty);
+                name.and_then(|name| fty.and_then(|fty| Ok((name.into(), fty))))
+            })
+            .collect::<Result<Vec<_>, String>>()?;
+
+        Ok(ManifestStructDef {
+            name,
+            canon_path,
+            fields,
+        })
+    }
+}
+
+enum ManifestType {
+    U8,
+    U16,
+    U32,
+    U64,
+    I8,
+    I16,
+    I32,
+    I64,
+    Bool,
+    StringLiteral,
+    Array(Box<ManifestType>, usize),
+    Unit,
+    Custom(String),
+}
+
+impl ManifestType {
+    fn from_ty(st: &StringTable, ty: &Type) -> Result<Self, String> {
+        let man_ty = match ty {
+            Type::U8 => Self::U8,
+            Type::U16 => Self::U16,
+            Type::U32 => Self::U32,
+            Type::U64 => Self::U64,
+            Type::I8 => Self::I8,
+            Type::I16 => Self::I16,
+            Type::I32 => Self::I32,
+            Type::I64 => Self::I64,
+            Type::Bool => Self::Bool,
+            Type::StringLiteral => Self::StringLiteral,
+            Type::Array(box el_ty, sz) => Self::Array(box Self::from_ty(st, el_ty)?, *sz),
+            Type::Unit => Self::Unit,
+            Type::Custom(p) => Self::Custom(path_to_string(st, p)?),
+            _ => {
+                return Err(format!(
+                    "Type cannot be converted to manifest type: {:?}",
+                    ty
+                ))
+            }
+        };
+
+        Ok(man_ty)
+    }
+}
+
+fn path_to_string(st: &StringTable, p: &Path) -> Result<String, String> {
+    let ps = p
+        .iter()
+        .map(|e| match e {
+            crate::compiler::ast::Element::FileRoot => Some("root"),
+            crate::compiler::ast::Element::CanonicalRoot => Some("project"),
+            crate::compiler::ast::Element::Selph => Some("self"),
+            crate::compiler::ast::Element::Super => Some("super"),
+            crate::compiler::ast::Element::Id(id) => st.get(*id),
+        })
+        .collect::<Option<Vec<&str>>>()
+        .ok_or(format!("Failed to convert Path to String"))?
+        .join("::");
+    Ok(ps)
 }
