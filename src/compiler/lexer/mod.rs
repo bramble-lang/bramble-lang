9ff7b3bcf79167019fd9db61ad5b700726b3cc38
Erich Ess <erich.g.ess@gmail.com>
2021-10-05 17:15:13 -0400

    Moved the Lexer errors into a file named error.rs; this follows the organization used in all other compiler submodules.  Making it much easier to know where to look for the errors for the Lexer

--- src/compiler/lexer/mod.rs
+++ src/compiler/lexer/mod.rs
@@ -1,36 +1,11 @@
-use tokens::Primitive;
-
-use super::{CompilerDisplay, CompilerDisplayError, CompilerError};
+use super::CompilerError;
 
 mod tests;
 
+mod error;
 pub(crate) mod lexer;
 pub(crate) mod tokens;
 
+pub use error::*;
+
 type LexerResult<T> = Result<T, CompilerError<LexerError>>;
-
-/// Errors which can be encountered while tokenizing a compilation unit
-#[derive(Clone, Debug, PartialEq)]
-pub enum LexerError {
-    Locked(Option<char>),
-    InvalidEscapeSequence(char),
-    ExpectedEscapeCharacter,
-    InvalidInteger,
-    UnexpectedSuffixType(Primitive),
-}
-
-impl CompilerDisplay for LexerError {
-    fn fmt(&self, st: &crate::StringTable) -> Result<String, CompilerDisplayError> {
-        use LexerError::*;
-        let msg = match self {
-            Locked(None) => format!("Lexer locked on EOF"),
-            Locked(Some(c)) => format!("Lexer locked on {}", c),
-            InvalidEscapeSequence(c) => format!("Invalid escape sequence \\{}", c),
-            ExpectedEscapeCharacter => format!("Expected an escape character after \\"),
-            InvalidInteger => format!("Invalid integer"),
-            UnexpectedSuffixType(ref prim) => format!("Invalid type suffix: {}", prim.fmt(st)?),
-        };
-
-        Ok(format!("{}", msg))
-    }
-}
