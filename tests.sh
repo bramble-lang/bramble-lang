f0fd15b41d0b9e43d7b0e5a7f793213b05bbc354
Erich Ess <erich.g.ess@gmail.com>
2021-10-18 13:40:59 -0400

    Update the test script to run unit tests

--- tests.sh
+++ tests.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
-# Runs the entire suite of Braid integration tests:
+# Runs the entire suite of Braid tests and return the number of test suites that
+# failed.  So, 0 will correspond, correctly, to success:
+#  0. Run the Unit Tests
 #  1. The Braid language test suite
 #  2. The Braid project import test suitee
 #  3. The Braid fuzzy syntax correctness test suite
 
-cd ./test
 ret=0
+cargo test --release
+if [[ $? -ne 0 ]]; then 
+    ret=$(($ret + 1))
+fi
+
+cd ./test
 
 ./test-llvm.sh
 if [[ $? -ne 0 ]]; then 
-    ret=1
+    ret=$(($ret + 1))
 fi
 
 ./test-imports.sh
 if [[ $? -ne 0 ]]; then 
-    ret=2
+    ret=$(($ret + 1))
 fi
 
 ./syntax-test.sh 25
 if [[ $? -ne 0 ]]; then 
-    ret=3
+    ret=$(($ret + 1))
 fi
 
 exit $ret
