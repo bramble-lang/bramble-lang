517df686a59308b314c4b04602184e0096d28b91
Erich Ess <erich.g.ess@gmail.com>
2022-01-18 14:39:48 -0500

    Deleting dead code

--- src/compiler/diagnostics/view.rs
+++ src/compiler/diagnostics/view.rs
@@ -27,16 +27,6 @@ impl<V, E> View<V> for Result<Option<V>, E> {
 }
 
 impl<V, E: CompilerDisplay + std::fmt::Debug> View2<V, E> for Result<Option<V>, CompilerError<E>> {
-    /*fn view2<F: FnOnce(Event<M,E>, &V), G: FnOnce(Event<M, E>, &CompilerError<E>)>(self, e: Event<M, E>, f: F, g: G) -> Self {
-        match &self {
-            Ok(Some(v)) => f(e, v),
-            Ok(None) => (),
-            Err(err) => g(e, err),
-        }
-
-        self
-    }*/
-
     fn view<F: FnOnce(Result<&V, &CompilerError<E>>)>(self, f: F) -> Self {
         match &self {
             Ok(Some(v)) => f(Ok(v)),
@@ -47,17 +37,6 @@ impl<V, E: CompilerDisplay + std::fmt::Debug> View2<V, E> for Result<Option<V>, 
         self
     }
 }
-
-/*impl<V, M: Writable, E: CompilerDisplay + std::fmt::Debug> View2<V, M, E> for Result<V, E> {
-    fn view2<F: FnOnce(Event<M,E>, &V), G: FnOnce(Event<M, E>, &E)>(self, e: Event<M, E>, f: F, g: G) -> Self {
-        match &self {
-            Ok(v) => f(e, v),
-            Err(err) => g(e, err),
-        }
-
-        self
-    }
-}*/
 
 impl<V, E> ViewErr<E> for Result<V, E> {
     fn view_err<F: Fn(&E)>(self, f: F) -> Self {
