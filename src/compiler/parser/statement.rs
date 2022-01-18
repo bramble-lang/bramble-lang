a1bc10d0c1fe98d7c321ba48243c14c8339d331d
Erich Ess <erich.g.ess@gmail.com>
2022-01-18 13:06:23 -0500

    WIP: statement

--- src/compiler/parser/statement.rs
+++ src/compiler/parser/statement.rs
@@ -1,11 +1,7 @@
 use super::{Parser, ParserResult};
 use crate::{
     compiler::{
-        ast::*,
-        diagnostics::{View, ViewErr},
-        lexer::tokens::Lex,
-        source::SourceIr,
-        CompilerError, Span,
+        ast::*, diagnostics::View2, lexer::tokens::Lex, source::SourceIr, CompilerError, Span,
     },
     StringId,
 };
@@ -32,202 +28,215 @@ impl<'a> Parser<'a> {
         &self,
         stream: &mut TokenStream,
     ) -> ParserResult<Statement<ParserContext>> {
-        let event = self.new_event(Span::zero());
-        let start_index = stream.index();
-        let must_have_semicolon = stream.test_if_one_of(vec![Lex::Let, Lex::Mut]);
-        let stm = match self.let_bind(stream)? {
-            Some(bind) => Some(Statement::Bind(Box::new(bind))),
-            None => match self.mutate(stream)? {
-                Some(mutate) => Some(Statement::Mutate(Box::new(mutate))),
-                None => self
-                    .expression(stream)?
-                    .map(|s| Statement::from_ast(s))
-                    .flatten(),
-            },
-        };
+        let (event, result) = self.new_event(Span::zero()).and_then(|| {
+            let start_index = stream.index();
+            let must_have_semicolon = stream.test_if_one_of(vec![Lex::Let, Lex::Mut]);
+            let stm = match self.let_bind(stream)? {
+                Some(bind) => Some(Statement::Bind(Box::new(bind))),
+                None => match self.mutate(stream)? {
+                    Some(mutate) => Some(Statement::Mutate(Box::new(mutate))),
+                    None => self
+                        .expression(stream)?
+                        .map(|s| Statement::from_ast(s))
+                        .flatten(),
+                },
+            };
 
-        match stm {
-            Some(mut stm) => match stream.next_if(&Lex::Semicolon) {
-                Some(semicolon) => {
-                    let ctx = stm.context().join(semicolon.to_ctx());
-                    *stm.get_context_mut() = ctx;
-                    Ok(Some(stm))
-                }
-                _ => {
-                    if must_have_semicolon {
-                        err!(
-                            stm.span(),
-                            ParserError::ExpectedButFound(
-                                vec![Lex::Semicolon],
-                                stream.peek().map(|x| x.sym.clone())
-                            )
-                        )
-                    } else {
-                        stream.set_index(start_index);
-                        Ok(None)
+            match stm {
+                Some(mut stm) => match stream.next_if(&Lex::Semicolon) {
+                    Some(semicolon) => {
+                        let ctx = stm.context().join(semicolon.to_ctx());
+                        *stm.get_context_mut() = ctx;
+                        Ok(Some(stm))
                     }
+                    _ => {
+                        if must_have_semicolon {
+                            err!(
+                                stm.span(),
+                                ParserError::ExpectedButFound(
+                                    vec![Lex::Semicolon],
+                                    stream.peek().map(|x| x.sym.clone())
+                                )
+                            )
+                        } else {
+                            stream.set_index(start_index);
+                            Ok(None)
+                        }
+                    }
+                },
+                None => {
+                    stream.set_index(start_index);
+                    Ok(None)
                 }
-            },
-            None => {
-                stream.set_index(start_index);
-                Ok(None)
             }
-        }
-        .view(|v| {
-            let msg = match &v {
+        });
+        result.view3(|v| {
+            let msg = v.map(|v| match &v {
                 Statement::Bind(..) => "Statement Bind",
                 Statement::Expression(..) => "Statement Expression",
                 Statement::Mutate(..) => "Statement Mutate",
                 Statement::Return(..) => "Statement Return",
                 Statement::YieldReturn(..) => "Statement Yield Return",
-            };
-            self.record(event.with_span(v.span()), Ok(msg))
+            });
+            self.record(event.with_span(v.span()), msg)
         })
-        //.view_err(|err| self.record(event.with_span(err.span()), Err(&err)))
     }
 
     fn let_bind(&self, stream: &mut TokenStream) -> ParserResult<Bind<ParserContext>> {
-        let event = self.new_event(Span::zero());
-        match stream.next_if(&Lex::Let) {
-            Some(let_tok) => {
-                let is_mutable = stream.next_if(&Lex::Mut).is_some();
-                self.id_declaration(stream)?
-                    .ok_or(CompilerError::new(
-                        let_tok.span(),
-                        ParserError::ExpectedIdDeclAfterLet,
-                    ))
-                    //.view_err(|err| self.record(event.with_span(err.span()), Err(&err)))?;
-                    .and_then(|id_decl| {
-                        stream.next_must_be(&Lex::Assign)?;
-
-                        let exp = match self.co_init(stream)? {
-                            Some(co_init) => co_init,
-                            None => self.expression(stream)?.ok_or(CompilerError::new(
+        let (event, result) =
+            self.new_event(Span::zero())
+                .and_then(|| match stream.next_if(&Lex::Let) {
+                    Some(let_tok) => {
+                        let is_mutable = stream.next_if(&Lex::Mut).is_some();
+                        self.id_declaration(stream)?
+                            .ok_or(CompilerError::new(
                                 let_tok.span(),
-                                ParserError::ExpectedExpressionOnRhs,
-                            ))?,
-                            /*.view_err(|err| {
-                                self.record(event.with_span(err.span()), Err(&err))
-                            })?, */
-                        };
+                                ParserError::ExpectedIdDeclAfterLet,
+                            ))
+                            .and_then(|id_decl| {
+                                stream.next_must_be(&Lex::Assign)?;
 
-                        match id_decl {
-                            Expression::IdentifierDeclare(_, id, ty) => {
-                                let ctx = exp.context().join(let_tok.to_ctx());
-                                Ok(Some(Bind::new(ctx, id, ty.clone(), is_mutable, exp)))
-                            }
-                            _ => Err(CompilerError::new(
-                                let_tok.span(),
-                                ParserError::ExpectedTypeInIdDecl,
-                            )),
-                        }
-                    })
-                    .view(|v| self.record(event.with_span(v.span()), Ok("Let Binding")))
-                //.view_err(|err| self.record(event.with_span(err.span()), Err(&err)))
-            }
-            None => Ok(None),
-        }
+                                let exp = match self.co_init(stream)? {
+                                    Some(co_init) => co_init,
+                                    None => self.expression(stream)?.ok_or(CompilerError::new(
+                                        let_tok.span(),
+                                        ParserError::ExpectedExpressionOnRhs,
+                                    ))?,
+                                };
+
+                                match id_decl {
+                                    Expression::IdentifierDeclare(_, id, ty) => {
+                                        let ctx = exp.context().join(let_tok.to_ctx());
+                                        Ok(Some(Bind::new(ctx, id, ty.clone(), is_mutable, exp)))
+                                    }
+                                    _ => Err(CompilerError::new(
+                                        let_tok.span(),
+                                        ParserError::ExpectedTypeInIdDecl,
+                                    )),
+                                }
+                            })
+                    }
+                    None => Ok(None),
+                });
+        result.view3(|v| {
+            let msg = v.map(|_| "Let Binding");
+            self.record(event.with_span(v.span()), msg)
+        })
     }
 
     fn mutate(&self, stream: &mut TokenStream) -> ParserResult<Mutate<ParserContext>> {
-        let event = self.new_event(Span::zero());
-        match stream.next_ifn(vec![
-            Lex::Mut,
-            Lex::Identifier(StringId::new()),
-            Lex::Assign,
-        ]) {
-            None => Ok(None),
-            Some(tokens) => {
-                let id = tokens[1]
-                    .sym
-                    .get_str()
-                    .expect("CRITICAL: identifier token cannot be converted to string");
-                self.expression(stream)?
-                    .ok_or(CompilerError::new(
-                        tokens[2].span(),
-                        ParserError::ExpectedExpressionOnRhs,
-                    ))
-                    //.view_err(|err| self.record(event.with_span(err.span()), Err(&err)))?;
-                    .and_then(|exp| {
-                        let ctx = tokens[0].to_ctx().join(*exp.context());
-                        Ok(Some(Mutate::new(ctx, id, exp)))
-                    })
+        let (event, result) = self.new_event(Span::zero()).and_then(|| {
+            match stream.next_ifn(vec![
+                Lex::Mut,
+                Lex::Identifier(StringId::new()),
+                Lex::Assign,
+            ]) {
+                None => Ok(None),
+                Some(tokens) => {
+                    let id = tokens[1]
+                        .sym
+                        .get_str()
+                        .expect("CRITICAL: identifier token cannot be converted to string");
+                    self.expression(stream)?
+                        .ok_or(CompilerError::new(
+                            tokens[2].span(),
+                            ParserError::ExpectedExpressionOnRhs,
+                        ))
+                        .and_then(|exp| {
+                            let ctx = tokens[0].to_ctx().join(*exp.context());
+                            Ok(Some(Mutate::new(ctx, id, exp)))
+                        })
+                }
             }
-        }
-        .view(|v| self.record(event.with_span(v.span()), Ok("Mutate")))
+        });
+        result.view3(|v| {
+            let msg = v.map(|_| "Mutate");
+            self.record(event.with_span(v.span()), msg)
+        })
     }
 
     fn co_init(&self, stream: &mut TokenStream) -> ParserResult<Expression<ParserContext>> {
-        let event = self.new_event(Span::zero());
-        match stream.next_if(&Lex::Init) {
-            Some(init_tok) => match self.path(stream)? {
-                Some((path, path_ctx)) => self
-                    .routine_call_params(stream)?
-                    .ok_or(CompilerError::new(
-                        path_ctx.span(),
-                        ParserError::ExpectedParams,
-                    ))
-                    .and_then(|(params, params_ctx)| {
-                        Ok(Some(Expression::RoutineCall(
-                            init_tok.to_ctx().join(params_ctx),
-                            RoutineCall::CoroutineInit,
-                            path,
-                            params,
-                        )))
-                    }),
-                None => Err(CompilerError::new(
-                    init_tok.span(),
-                    ParserError::ExpectedIdAfterInit,
-                )),
-            },
-            _ => Ok(None),
-        }
-        .view(|v| self.record(event.with_span(v.span()), Ok("Coroutine Init")))
-        //.view_err(|err| self.record(event.with_span(err.span()), Err(&err)))
+        let (event, result) =
+            self.new_event(Span::zero())
+                .and_then(|| match stream.next_if(&Lex::Init) {
+                    Some(init_tok) => match self.path(stream)? {
+                        Some((path, path_ctx)) => self
+                            .routine_call_params(stream)?
+                            .ok_or(CompilerError::new(
+                                path_ctx.span(),
+                                ParserError::ExpectedParams,
+                            ))
+                            .and_then(|(params, params_ctx)| {
+                                Ok(Some(Expression::RoutineCall(
+                                    init_tok.to_ctx().join(params_ctx),
+                                    RoutineCall::CoroutineInit,
+                                    path,
+                                    params,
+                                )))
+                            }),
+                        None => Err(CompilerError::new(
+                            init_tok.span(),
+                            ParserError::ExpectedIdAfterInit,
+                        )),
+                    },
+                    _ => Ok(None),
+                });
+        result.view3(|v| {
+            let msg = v.map(|_| "Coroutine Init");
+            self.record(event.with_span(v.span()), msg)
+        })
     }
 
     pub(super) fn return_stmt(
         &self,
         stream: &mut TokenStream,
     ) -> ParserResult<Return<ParserContext>> {
-        let event = self.new_event(Span::zero());
-        Ok(match stream.next_if(&Lex::Return) {
-            Some(token) => {
-                let exp = self.expression(stream)?;
-                stream.next_must_be(&Lex::Semicolon)?;
-                match exp {
-                    Some(exp) => {
-                        let ctx = token.to_ctx().join(*exp.context());
-                        Some(Return::new(ctx, Some(exp)))
+        let (event, result) = self.new_event(Span::zero()).and_then(|| {
+            Ok(match stream.next_if(&Lex::Return) {
+                Some(token) => {
+                    let exp = self.expression(stream)?;
+                    stream.next_must_be(&Lex::Semicolon)?;
+                    match exp {
+                        Some(exp) => {
+                            let ctx = token.to_ctx().join(*exp.context());
+                            Some(Return::new(ctx, Some(exp)))
+                        }
+                        None => Some(Return::new(token.to_ctx(), None)),
                     }
-                    None => Some(Return::new(token.to_ctx(), None)),
                 }
-            }
-            _ => None,
+                _ => None,
+            })
+        });
+        result.view3(|v| {
+            let msg = v.map(|_| "Return");
+            self.record(event.with_span(v.span()), msg)
         })
-        .view(|v| self.record(event.with_span(v.span()), Ok("Return")))
     }
 
     fn yield_return_stmt(
         &self,
         stream: &mut TokenStream,
     ) -> ParserResult<Statement<ParserContext>> {
-        let event = self.new_event(Span::zero());
-        Ok(match stream.next_if(&Lex::YieldReturn) {
-            Some(token) => {
-                let exp = self.expression(stream)?;
-                stream.next_must_be(&Lex::Semicolon)?;
-                let yret = match exp {
-                    Some(exp) => {
-                        let ctx = token.to_ctx().join(*exp.context());
-                        YieldReturn::new(ctx, Some(exp))
-                    }
-                    None => YieldReturn::new(token.to_ctx(), None),
-                };
-                Some(Statement::YieldReturn(Box::new(yret)))
-            }
-            _ => None,
+        let (event, result) = self.new_event(Span::zero()).and_then(|| {
+            Ok(match stream.next_if(&Lex::YieldReturn) {
+                Some(token) => {
+                    let exp = self.expression(stream)?;
+                    stream.next_must_be(&Lex::Semicolon)?;
+                    let yret = match exp {
+                        Some(exp) => {
+                            let ctx = token.to_ctx().join(*exp.context());
+                            YieldReturn::new(ctx, Some(exp))
+                        }
+                        None => YieldReturn::new(token.to_ctx(), None),
+                    };
+                    Some(Statement::YieldReturn(Box::new(yret)))
+                }
+                _ => None,
+            })
+        });
+        result.view3(|v| {
+            let msg = v.map(|_| "Yield Return");
+            self.record(event.with_span(v.span()), msg)
         })
-        .view(|v| self.record(event.with_span(v.span()), Ok("Yield Return")))
     }
 }
