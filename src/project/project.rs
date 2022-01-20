9be6ca9de9d5675bd5fd6d550fca27241f097ede
Erich Ess <erich.g.ess@gmail.com>
2022-01-20 10:53:58 -0500

    Explanatory comment

--- src/project/project.rs
+++ src/project/project.rs
@@ -99,7 +99,7 @@ pub fn build_source_map(
     let mut sm = SourceMap::new();
 
     let mut files = get_files(&src_path, ext)?;
-    files.sort();
+    files.sort(); // simplifies testing across platforms by making the source map consistent
     for file in files {
         sm.add_file(file)?;
     }
