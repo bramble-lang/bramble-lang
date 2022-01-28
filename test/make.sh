2c1e04b9bfb9ce2aedd6af0eea48ac8d22acc37e
Erich Ess <erich.g.ess@gmail.com>
2022-01-28 12:53:03 -0500

    Pointer target types were not getting canonized. Removed wild card match on the canonize_type function so that the compiler will catch if a new type is not canonized

--- test/make.sh
+++ test/make.sh
@@ -4,13 +4,22 @@ run() {
     # test=$1
     rm -rf ./target
     mkdir -p ./target
+    target=--release
+
+    if [[ "$BRAID_TARGET" == "debug" ]]; then
+        target=
+    elif [[ "$BRAID_TARGET" == "release" ]]; then
+        target=--release
+    fi
+
+    echo $target
 
     built=0
 
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         echo "Compiling"
-        cargo run --release --bin braidc -- --llvm -p linux --input "../braid/std" -o ./target/std.obj --manifest
-        cargo run --release --bin braidc -- --llvm -p linux --import ./target/std.manifest "$@" -o ./target/output.obj
+        cargo run $target --bin braidc -- --llvm -p linux --input "../braid/std" -o ./target/std.obj --manifest
+        cargo run $target --bin braidc -- --llvm -p linux --import ./target/std.manifest "$@" -o ./target/output.obj
         echo ""
         echo "Assembling"
         nasm -g -f elf64 ../braid/linux/llvm/std/input.asm -l ./target/std_input.lst -o ./target/std_input.obj > assembler.log
@@ -18,8 +27,8 @@ run() {
         built=1
     elif [[ "$OSTYPE" == "darwin"* ]]; then
         echo "Compiling"
-        cargo run --release --bin braidc -- --llvm -p machos --input "../braid/std" -o ./target/std.obj --manifest
-        cargo run --release --bin braidc -- --llvm -p machos --import ./target/std.manifest "$@" -o ./target/output.obj
+        cargo run --$target --bin braidc -- --llvm -p machos --input "../braid/std" -o ./target/std.obj --manifest
+        cargo run --$target --bin braidc -- --llvm -p machos --import ./target/std.manifest "$@" -o ./target/output.obj
         echo ""
         echo "Assembling"
         nasm -g -f macho64 ../braid/macho64/llvm/std/input.asm -l ./target/std_input.lst -o ./target/std_input.obj > assembler.log
