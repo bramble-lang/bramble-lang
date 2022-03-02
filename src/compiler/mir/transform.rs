36a2a63ff70f2777514af84a0d74eebdb4872211
Erich Ess <erich.g.ess@gmail.com>
2022-03-02 13:53:28 -0500

    WIP MIR Xfmr: will now run through the functions in a module and then print out the debug for that MIR value. This will allow for end-to-end test of a simple function

--- src/compiler/mir/transform.rs
+++ src/compiler/mir/transform.rs
@@ -9,34 +9,50 @@
 
 use log::debug;
 
-use crate::compiler::{ast::{RoutineDef, Expression, Statement, BinaryOperator, Type}, semantics::semanticnode::SemanticContext};
+use crate::compiler::{
+    ast::{BinaryOperator, Expression, Module, RoutineDef, Statement, Type},
+    semantics::semanticnode::SemanticContext,
+    Span,
+};
 
 use super::ir::*;
 
-/// Provides a Builder interface for constructing the MIR CFG representation of a 
+pub fn module_transform(module: &Module<SemanticContext>) {
+    let funcs = module.get_functions();
+
+    for f in funcs {
+        match f {
+            crate::compiler::ast::Item::Routine(r) => {
+                let ft = FuncTransformer::new();
+                let p = ft.transform(r);
+                println!("{:?}", p);
+            }
+            crate::compiler::ast::Item::Struct(_) => todo!(),
+            crate::compiler::ast::Item::Extern(_) => todo!(),
+        }
+    }
+}
+
+/// Provides a Builder interface for constructing the MIR CFG representation of a
 /// routine. This will keep track of the current [`BasicBlock`] and make sure that
 /// MIR operations are applied to that [`BasicBlock`]. This also provides a simplfied
-/// interface for constructing the MIR operands, operations, and statements, to 
+/// interface for constructing the MIR operands, operations, and statements, to
 /// simplify the code that traverses input ASTs and transforms them into MIR.
-pub struct MirGenerator {
-    proc: Option<Procedure>,
+struct MirGenerator {
+    proc: Procedure,
     current_bb: Option<BasicBlockId>,
 }
 
 impl MirGenerator {
     pub fn new() -> MirGenerator {
-        todo!()
+        MirGenerator {
+            proc: Procedure::new(&Type::Unit, Span::zero()),
+            current_bb: None,
+        }
     }
 
-    fn module(&self) {}
-
     fn new_bb(&mut self) -> BasicBlockId {
-        match &mut self.proc {
-            Some(p) => {
-                p.new_bb()
-            },
-            None => todo!(),
-        }
+        self.proc.new_bb()
     }
 
     fn set_bb(&mut self, bb: BasicBlockId) {
@@ -52,10 +68,7 @@ impl MirGenerator {
     }
 
     fn temp(&mut self, ty: &Type) -> TempId {
-        match &mut self.proc {
-            Some(p) => p.add_temp(ty),
-            None => todo!(),
-        }
+        self.proc.add_temp(ty)
     }
 
     fn temp_store(&mut self, rv: RValue, ty: &Type) -> Operand {
@@ -63,10 +76,11 @@ impl MirGenerator {
         debug!("Temp store: {:?} := {:?}", tv, rv);
 
         let cid = self.current_bb.unwrap();
-        self.proc.as_mut().map(|p| {
-            let bb = p.get_bb_mut(cid);
-            bb.add_stm(super::ir::Statement::new(StatementKind::Assign(tv.clone(), rv)))
-        });
+        let bb = self.proc.get_bb_mut(cid);
+        bb.add_stm(super::ir::Statement::new(StatementKind::Assign(
+            tv.clone(),
+            rv,
+        )));
 
         Operand::LValue(tv)
     }
@@ -91,10 +105,8 @@ impl MirGenerator {
     fn term_return(&mut self) {
         debug!("Terminator: Return");
         let cid = self.current_bb.unwrap();
-        self.proc.as_mut().map(|p| {
-            let bb = p.get_bb_mut(cid);
-            bb.set_terminator(Terminator::new(TerminatorKind::Return))
-        });
+        let bb = self.proc.get_bb_mut(cid);
+        bb.set_terminator(Terminator::new(TerminatorKind::Return));
     }
 
     /// Terminates by going to the destination basic block
@@ -128,19 +140,21 @@ impl FuncTransformer {
         }
     }
 
-    pub fn transform(&mut self, func: RoutineDef<SemanticContext>) {
+    pub fn transform(mut self, func: &RoutineDef<SemanticContext>) -> Procedure {
         // Create a new MIR Procedure
         // Create a BasicBlock for the function
         let bb = self.gen.new_bb();
         self.gen.set_bb(bb);
 
         // Iterate over every statement and add it to the basic block
+        func.body.iter().for_each(|stm| self.statement(stm));
 
         // Add the return from function as the terminator for the final basic block of the function
         self.gen.term_return();
+        self.gen.proc
     }
 
-    fn statement(&mut self, stm: Statement<SemanticContext>) {
+    fn statement(&mut self, stm: &Statement<SemanticContext>) {
         match stm {
             Statement::Bind(bind) => todo!(),
             Statement::Expression(expr) => todo!(),
@@ -152,14 +166,15 @@ impl FuncTransformer {
 
     /// This can return either an Operand or an RValue, if this is evaluating a constant or an identifier
     /// then this returns an operand.  If this is evaluating an operation then it returns an RValue.
-    fn expression(&mut self, expr: &Expression<SemanticContext>) -> ExprResult { /// TEMP: it might turn out that I don't need ExprResult
+    fn expression(&mut self, expr: &Expression<SemanticContext>) -> ExprResult {
+        // TEMP: it might turn out that I don't need ExprResult
         match expr {
             Expression::I64(_, i) => ExprResult::Operand(self.gen.const_i64(*i)),
             Expression::BinaryOp(ctx, op, left, right) => {
                 let rv = self.binary_op(*op, left, right);
                 let temp = self.gen.temp_store(rv, ctx.ty());
                 ExprResult::Operand(temp)
-            },
+            }
             Expression::Null(_) => todo!(),
             Expression::U8(_, _) => todo!(),
             Expression::U16(_, _) => todo!(),
@@ -172,7 +187,11 @@ impl FuncTransformer {
             Expression::Boolean(_, _) => todo!(),
             Expression::StringLiteral(_, _) => todo!(),
             Expression::ArrayExpression(_, _, _) => todo!(),
-            Expression::ArrayAt { context, array, index } => todo!(),
+            Expression::ArrayAt {
+                context,
+                array,
+                index,
+            } => todo!(),
             Expression::SizeOf(_, _) => todo!(),
             Expression::CustomType(_, _) => todo!(),
             Expression::Identifier(_, _) => todo!(),
@@ -181,8 +200,17 @@ impl FuncTransformer {
             Expression::IdentifierDeclare(_, _, _) => todo!(),
             Expression::RoutineCall(_, _, _, _) => todo!(),
             Expression::StructExpression(_, _, _) => todo!(),
-            Expression::If { context, cond, if_arm, else_arm } => todo!(),
-            Expression::While { context, cond, body } => todo!(),
+            Expression::If {
+                context,
+                cond,
+                if_arm,
+                else_arm,
+            } => todo!(),
+            Expression::While {
+                context,
+                cond,
+                body,
+            } => todo!(),
             Expression::ExpressionBlock(_, _, _) => todo!(),
             Expression::TypeCast(_, _, _) => todo!(),
             Expression::UnaryOp(_, _, _) => todo!(),
@@ -190,13 +218,18 @@ impl FuncTransformer {
         }
     }
 
-    fn binary_op(&mut self, op: BinaryOperator, left: &Expression<SemanticContext>, right: &Expression<SemanticContext>) -> RValue {
+    fn binary_op(
+        &mut self,
+        op: BinaryOperator,
+        left: &Expression<SemanticContext>,
+        right: &Expression<SemanticContext>,
+    ) -> RValue {
         match op {
             BinaryOperator::Add => {
                 let left = self.expression(left).operand();
                 let right = self.expression(right).operand();
                 self.gen.add(left, right)
-            },
+            }
             BinaryOperator::Sub => todo!(),
             BinaryOperator::Mul => todo!(),
             BinaryOperator::Div => todo!(),
@@ -215,14 +248,14 @@ impl FuncTransformer {
 
 enum ExprResult {
     Operand(Operand),
-    RValue(RValue)
+    RValue(RValue),
 }
 
 impl ExprResult {
     fn operand(self) -> Operand {
         match self {
             Self::Operand(op) => op,
-            Self::RValue(_) => panic!("Expected Operand but got RValue")
+            Self::RValue(_) => panic!("Expected Operand but got RValue"),
         }
     }
 
