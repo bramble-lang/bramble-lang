cad2a838b3b65442d04872bf0a80616dccb98cdc
Erich Ess <erich.g.ess@gmail.com>
2022-03-02 12:21:41 -0500

    WIP: expanding a couple operations so that simple testing of AST->MIR transformation can be done

--- src/compiler/mir/transform.rs
+++ src/compiler/mir/transform.rs
@@ -7,13 +7,13 @@
 // CFG MIR used for dataflow analysis and LLVM IR generation by the Bramble
 // compiler.
 
-use crate::compiler::{ast::RoutineDef, semantics::semanticnode::SemanticContext};
+use crate::compiler::{ast::{RoutineDef, Expression, Statement, BinaryOperator}, semantics::semanticnode::SemanticContext};
 
-use super::ir::{BasicBlockId, VarId, TempId};
+use super::ir::*;
 
 /// Provides a Builder interface for constructing the MIR CFG representation of a 
-/// routine. This will keep track of the current [`super::ir::BasicBlock`] and make sure that
-/// MIR operations are applied to that [`super::ir::BasicBlock`]. This also provides a simplfied
+/// routine. This will keep track of the current [`BasicBlock`] and make sure that
+/// MIR operations are applied to that [`BasicBlock`]. This also provides a simplfied
 /// interface for constructing the MIR operands, operations, and statements, to 
 /// simplify the code that traverses input ASTs and transforms them into MIR.
 pub struct MirGenerator {}
@@ -30,6 +30,10 @@ impl MirGenerator {
     }
 
     fn set_bb(&mut self, bb: BasicBlockId) {
+        todo!()
+    }
+
+    fn const_i64(&mut self, i: i64) -> Operand {
         todo!()
     }
 
@@ -50,7 +54,7 @@ impl MirGenerator {
     }
 
     /// Add two operands together
-    fn add(&mut self) {
+    fn add(&mut self, left: Operand, right: Operand) -> RValue {
         todo!()
     }
 
@@ -93,9 +97,77 @@ impl FuncTransformer {
         let bb = self.gen.new_bb();
         self.gen.set_bb(bb);
 
-        // Iterate over every expression and add it to the basic block
+        // Iterate over every statement and add it to the basic block
 
         // Add the return from function as the terminator for the final basic block of the function
         self.gen.term_return();
+    }
+
+    fn statement(&mut self, stm: Statement<SemanticContext>) {
+        match stm {
+            Statement::Bind(bind) => todo!(),
+            Statement::Expression(expr) => todo!(),
+            Statement::Mutate(_) => todo!(),
+            Statement::YieldReturn(_) => todo!(),
+            Statement::Return(ret) => todo!(),
+        }
+    }
+
+    /// This can return either an Operand or an RValue, if this is evaluating a constant or an identifier
+    /// then this returns an operand.  If this is evaluating an operation then it returns an RValue.
+    fn expression(&mut self, expr: Expression<SemanticContext>) -> Result<Operand, RValue> { // TEMP using Result as Rust does not have an Either builtin
+        match expr {
+            Expression::Null(_) => todo!(),
+            Expression::U8(_, _) => todo!(),
+            Expression::U16(_, _) => todo!(),
+            Expression::U32(_, _) => todo!(),
+            Expression::U64(_, _) => todo!(),
+            Expression::I8(_, _) => todo!(),
+            Expression::I16(_, _) => todo!(),
+            Expression::I32(_, _) => todo!(),
+            Expression::I64(_, i) => Ok(self.gen.const_i64(i)),
+            Expression::F64(_, _) => todo!(),
+            Expression::Boolean(_, _) => todo!(),
+            Expression::StringLiteral(_, _) => todo!(),
+            Expression::ArrayExpression(_, _, _) => todo!(),
+            Expression::ArrayAt { context, array, index } => todo!(),
+            Expression::SizeOf(_, _) => todo!(),
+            Expression::CustomType(_, _) => todo!(),
+            Expression::Identifier(_, _) => todo!(),
+            Expression::Path(_, _) => todo!(),
+            Expression::MemberAccess(_, _, _) => todo!(),
+            Expression::IdentifierDeclare(_, _, _) => todo!(),
+            Expression::RoutineCall(_, _, _, _) => todo!(),
+            Expression::StructExpression(_, _, _) => todo!(),
+            Expression::If { context, cond, if_arm, else_arm } => todo!(),
+            Expression::While { context, cond, body } => todo!(),
+            Expression::ExpressionBlock(_, _, _) => todo!(),
+            Expression::BinaryOp(_, _, _, _) => todo!(),
+            Expression::TypeCast(_, _, _) => todo!(),
+            Expression::UnaryOp(_, _, _) => todo!(),
+            Expression::Yield(_, _) => todo!(),
+        }
+    }
+
+    fn binary_op(&mut self, op: BinaryOperator, left: Expression<SemanticContext>, right: Expression<SemanticContext>) -> RValue {
+        match op {
+            BinaryOperator::Add => {
+                let left = self.expression(left);
+                let right = self.expression(right);
+                self.gen.add(left, right)
+            },
+            BinaryOperator::Sub => todo!(),
+            BinaryOperator::Mul => todo!(),
+            BinaryOperator::Div => todo!(),
+            BinaryOperator::BAnd => todo!(),
+            BinaryOperator::BOr => todo!(),
+            BinaryOperator::Eq => todo!(),
+            BinaryOperator::NEq => todo!(),
+            BinaryOperator::Ls => todo!(),
+            BinaryOperator::LsEq => todo!(),
+            BinaryOperator::Gr => todo!(),
+            BinaryOperator::GrEq => todo!(),
+            BinaryOperator::RawPointerOffset => todo!(),
+        }
     }
 }
