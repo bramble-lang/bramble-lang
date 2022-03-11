22d66c172e2cb1d75bbc1a91fb366fca5b36068c
Erich Ess <erich.g.ess@gmail.com>
2022-03-11 15:32:58 -0500

    Skeleton code

--- src/compiler/mir/project.rs
+++ src/compiler/mir/project.rs
@@ -1,12 +1,16 @@
-//! Represents and entire Bramble program, including imported libraries,
-//! in MIR form.
+/*! Represents and entire Bramble program, including imported libraries,
+in MIR form.
+*/
 
-
-/// Uniquely identifies an item that exists in the static memory of a program
-/// e.g., a function or static variable.
-struct DefId;
+/// Represents everything involved in compiling the current target compilation
+/// unit (executable, library, etc.).
+/// 
+/// This corresponds to the Root of the Canonical Path
+struct MirProgram;
 
 /// The representation of the full program, including imports
+/// 
+/// This corresponds to the Project component of the Canonical Path
 struct MirProject;
 
 /// Represents definitions of static items within this project
@@ -14,3 +18,10 @@ struct MirProject;
 /// This also includes static strings.
 struct Definitions;
 
+/// Uniquely identifies an item that exists in the static memory of a program
+/// e.g., a function or static variable.
+struct DefId;
+
+/// A static item, this could be a function or data.  Data in turn can be a
+/// variable or a constant.
+struct StaticItem;
