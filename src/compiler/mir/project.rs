9da5872d0a605557976c5d61306ab45b0d49a550
Erich Ess <erich.g.ess@gmail.com>
2022-03-14 10:27:59 -0400

    Test overwriting functions

--- src/compiler/mir/project.rs
+++ src/compiler/mir/project.rs
@@ -122,7 +122,7 @@ impl StaticDefinitions {
 
 /// Uniquely identifies an item that exists in the static memory of a program
 /// e.g., a function or static variable.
-#[derive(Debug, Copy, Clone, Default)]
+#[derive(PartialEq, Debug, Copy, Clone, Default)]
 pub struct DefId(u32);
 
 impl DefId {
--- src/compiler/mir/project_test.rs
+++ src/compiler/mir/project_test.rs
@@ -27,6 +27,30 @@ mod static_defs {
         assert_eq!(actual, &StaticItem::Function(func));
     }
 
+    #[test]
+    fn overwrite_func() {
+        // Create a static defs table
+        let mut proj = MirProject::new();
+
+        // Add a procedure to the table
+        let path: Path = vec![Element::CanonicalRoot, Element::Id(StringId::new())].into();
+        let mut func = Procedure::new(&path, &Type::Unit, Span::zero());
+
+        // Get the procedure with the given def id
+        let id = proj.add_func(func.clone()).unwrap();
+
+        // Change the function
+        func.add_temp(&Type::U32, Span::zero());
+        let id2 = proj.add_func(func.clone()).unwrap();
+
+        // Check that same DefId is returned
+        assert_eq!(id, id2);
+
+        // Assert that the function got overwritten
+        let actual = proj.get_def(id);
+        assert_eq!(actual, &StaticItem::Function(func));
+    }
+
     fn find_func() {
         // Add a func to the static def table
         // Find with canonical path
