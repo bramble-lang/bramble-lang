d353eb3951fab243350ecdfabeb699fc6e9b9eab
Erich Ess <erich.g.ess@gmail.com>
2022-03-14 12:26:54 -0400

    Add unit tests

--- src/compiler/mir/project_test.rs
+++ src/compiler/mir/project_test.rs
@@ -1,5 +1,6 @@
 #![cfg(test)]
 
+/// Unit Tests for the Static Definition table.
 mod static_defs {
     use crate::{
         compiler::{
@@ -66,5 +67,23 @@ mod static_defs {
         let actual_id = proj.find_def(&path).unwrap();
 
         assert_eq!(actual_id, expected_id);
+    }
+
+    #[test]
+    fn find_func_none() {
+        let mut proj = MirProject::new();
+
+        // Add a procedure to the table
+        let path: Path = vec![Element::CanonicalRoot, Element::Id(StringId::new())].into();
+        let func = Procedure::new(&path, &Type::Unit, Span::zero());
+
+        // Get the procedure with the given def id
+        proj.add_func(func.clone()).unwrap();
+
+        // Search for the function
+        let mispath: Path = vec![Element::CanonicalRoot, Element::Id(StringId::new()), Element::Id(StringId::new())].into();
+        let actual_id = proj.find_def(&mispath);
+
+        assert_eq!(actual_id, None);
     }
 }
