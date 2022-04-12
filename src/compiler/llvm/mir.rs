f94d6dac3df8d2c7859c1aa51bc15f930c934d56
Erich Ess <erich.g.ess@gmail.com>
2022-04-12 10:24:01 -0400

    deleted unused code

--- src/compiler/llvm/mir.rs
+++ src/compiler/llvm/mir.rs
@@ -115,12 +115,6 @@ impl<'module, 'ctx> LlvmProgram<'module, 'ctx> {
         self.module.print_to_stderr()
     }
 
-    pub fn create_engine(
-        &self,
-    ) -> Result<inkwell::execution_engine::ExecutionEngine, inkwell::support::LLVMString> {
-        self.module.create_interpreter_execution_engine()
-    }
-
     /// Print the assembly representation of this Bramble program to `stdout`
     pub fn print_asm(&self) {
         // Get target for current machine
