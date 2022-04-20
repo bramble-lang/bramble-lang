3894cc4add5786408b15a8e255d4c74fe72cb899
Erich Ess <erich.g.ess@gmail.com>
2022-04-20 10:14:37 -0400

    Update secondary test scripts to use mir-beta pipeline

--- test/make-import_test.sh
+++ test/make-import_test.sh
@@ -1,35 +1,35 @@
 #!/bin/sh
 
 run() {
-    # test=$1
-    rm -rf ./target
-    mkdir -p ./target
+	# test=$1
+	rm -rf ./target
+	mkdir -p ./target
 
-    built=0
+	built=0
 
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        echo "Compiling"
-        ../target/debug/bramblec -p linux --input "./import/shared" -o ./target/shared.obj --manifest
-        ../target/debug/bramblec -p linux --import ./target/shared.manifest --input "./import/indirect" -o ./target/indirect.obj --manifest
-        ../target/debug/bramblec -p linux --input "../bramble/std" -o ./target/std.obj --manifest
-        ../target/debug/bramblec -p linux --import ./target/std.manifest,./target/shared.manifest,./target/indirect.manifest "$@" -o ./target/output.obj
-        echo ""
-        echo "Assembling"
-        gcc -no-pie -fno-pie -w ./target/indirect.obj ./target/shared.obj ./target/std.obj ./target/output.obj -g -o ./target/output -m64 2>&1 > gcc.log
-        built=1
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        echo "Compiling"
-        ../target/debug/bramblec -p machos --input "./import/shared" -o ./target/shared.obj --manifest
-        ../target/debug/bramblec -p machos --import ./target/shared.manifest --input "./import/indirect" -o ./target/indirect.obj --manifest
-        ../target/debug/bramblec -p machos --input "../bramble/std" -o ./target/std.obj --manifest
-        ../target/debug/bramblec -p machos --import ./target/std.manifest,./target/shared.manifest,./target/indirect.manifest "$@" -o ./target/output.obj
-        echo ""
-        echo "Assembling"
-        gcc -w ./target/indirect.obj ./target/shared.obj ./target/std.obj ./target/output.obj -g -o ./target/output -m64 2>&1 > gcc.log
-        built=1
-    else
-        echo "Unknown OS: ${OSTYPE}"
-    fi
+	if [[ $OSTYPE == "linux-gnu"* ]]; then
+		echo "Compiling"
+		../target/debug/bramblec --mir-beta -p linux --input "./import/shared" -o ./target/shared.obj --manifest
+		../target/debug/bramblec --mir-beta -p linux --import ./target/shared.manifest --input "./import/indirect" -o ./target/indirect.obj --manifest
+		../target/debug/bramblec --mir-beta -p linux --input "../bramble/std" -o ./target/std.obj --manifest
+		../target/debug/bramblec --mir-beta -p linux --import ./target/std.manifest,./target/shared.manifest,./target/indirect.manifest "$@" -o ./target/output.obj
+		echo ""
+		echo "Assembling"
+		gcc -no-pie -fno-pie -w ./target/indirect.obj ./target/shared.obj ./target/std.obj ./target/output.obj -g -o ./target/output -m64 2>&1 >gcc.log
+		built=1
+	elif [[ $OSTYPE == "darwin"* ]]; then
+		echo "Compiling"
+		../target/debug/bramblec -p machos --input "./import/shared" -o ./target/shared.obj --manifest
+		../target/debug/bramblec -p machos --import ./target/shared.manifest --input "./import/indirect" -o ./target/indirect.obj --manifest
+		../target/debug/bramblec -p machos --input "../bramble/std" -o ./target/std.obj --manifest
+		../target/debug/bramblec -p machos --import ./target/std.manifest,./target/shared.manifest,./target/indirect.manifest "$@" -o ./target/output.obj
+		echo ""
+		echo "Assembling"
+		gcc -w ./target/indirect.obj ./target/shared.obj ./target/std.obj ./target/output.obj -g -o ./target/output -m64 2>&1 >gcc.log
+		built=1
+	else
+		echo "Unknown OS: ${OSTYPE}"
+	fi
 }
 
 echo "Compile with LLVM ${@}"
--- test/test-syntax.sh
+++ test/test-syntax.sh
@@ -1,9 +1,8 @@
 #! /bin/sh
 # Randomly generate syntactically correct code and test that bramblec validates it.
-if [ -z "$1" ]
-  then
-    echo "No argument supplied"
-    exit 1
+if [ -z "$1" ]; then
+	echo "No argument supplied"
+	exit 1
 fi
 
 start_time=$(date +%s)
@@ -12,21 +11,21 @@ cargo build
 
 COUNTER=0
 until [ $COUNTER -ge $1 ]; do
-    let COUNTER+=1
-    echo $COUNTER
+	let COUNTER+=1
+	echo $COUNTER
 
-    rm /tmp/bramble_test.br
-    ../target/debug/bramble-gen 8 20 > /tmp/bramble_test.br
-    ../target/debug/bramblec --stage=parser --platform=linux -i /tmp/bramble_test.br --output=./
-    result=$?
+	rm /tmp/bramble_test.br
+	../target/debug/bramble-gen 8 20 >/tmp/bramble_test.br
+	../target/debug/bramblec --mir-beta --stage=parser --platform=linux -i /tmp/bramble_test.br --output=./
+	result=$?
 
-    if [[ result -ne 0 ]]; then
-        end_time=$(date +%s)
-        elapsed=$(( end_time - start_time ))
-        cat /tmp/bramble_test.br
-        exit 1
-    fi
+	if [[ result -ne 0 ]]; then
+		end_time=$(date +%s)
+		elapsed=$((end_time - start_time))
+		cat /tmp/bramble_test.br
+		exit 1
+	fi
 done
 end_time=$(date +%s)
-elapsed=$(( end_time - start_time ))
+elapsed=$((end_time - start_time))
 echo "Elapsed: ${elapsed}"
